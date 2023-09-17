3c1089faee266a656eb70183ff16576ea1e9ab84
Victor <123475829+Vinas-Baik@users.noreply.github.com>
2023-09-17 08:26:07 +0800

    Update create_tables.sql
    -- Создаем таблицы

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,3 +1,30 @@
 -- SQL-команды для создания таблиц
 -- Создаем базу north
 CREATE DATABASE north;
+
+-- Создаем таблицы 
+
+CREATE TABLE customers(
+	customer_id varchar(10) NOT NULL PRIMARY KEY,
+	company_name varchar(50) NOT NULL,
+	contact_name varchar(50)
+);
+
+CREATE TABLE employees(
+	employee_id int NOT NULL PRIMARY KEY, 
+	first_name varchar(50) NOT NULL, 
+	last_name varchar(50),
+	title varchar(100),
+	birth_date date,
+	notes text 
+); 
+
+-- отношение один к многим 
+
+CREATE TABLE orders(
+	order_id int NOT NULL PRIMARY KEY, 
+	customer_id varchar(10) NOT NULL REFERENCES customers(customer_id),
+	employee_id int NOT NULL REFERENCES employees(employee_id),
+	order_date date,
+	ship_city varchar(50)
+);
