6000c3fb647e4f36d922003f7c39c9f8a825ee52
Victor Sadykov <svn1409@gmail.com>
2023-09-17 10:15:21 +0800

    homework 1 def clear_database(my_db_tables):

--- homework-1/main.py
+++ homework-1/main.py
@@ -6,17 +6,17 @@ import os
 
 from utils.svn_utils import *
 
-TABLES = [{'file_name': os.getcwd() + 'north_data\\customers_data.csv',
+TABLES = [{'file_name': os.getcwd() + '\\north_data\\customers_data.csv',
            'table': 'customers',
            'values': 'customer_id, company_name, contact_name',
            'type_values': 'int, str, str'
            },
-          {'file_name': os.getcwd() + 'north_data\\orders_data.csv',
+          {'file_name': os.getcwd() + '\\north_data\\orders_data.csv',
            'table': 'orders',
            'values': 'order_id, customer_id, employee_id, order_date, ship_city',
            'type_values': 'int, str, int, date, str'
            },
-          {'file_name': os.getcwd() + 'north_data\\employees_data.csv',
+          {'file_name': os.getcwd() + '\\north_data\\employees_data.csv',
            'table': 'employees',
            'values': 'employee_id, first_name, last_name, title, birth_date, notes',
            'type_values': 'int, str, str, str, date, str'
@@ -27,8 +27,6 @@ DB_HOST = 'localhost'
 DB_BASE = 'north'
 DB_USER = 'postgres'
 DB_PASSWORD = '12345'
-
-
 
 
 def user_menu() -> str:
@@ -48,7 +46,40 @@ def user_menu() -> str:
 
 def load_from_csv_in_table(table):
 
+    with open(table['file_name'], 'r', encoding='UTF-8') as file:
+        rows = csv.reader(file)
+        # for item_table in table['values'].split(', '):
+        #     print(item_table, end=', ')
+        # print()
+        for row in rows:
+            for item_table in table['values'].split(', '):
+                print(f'\033[33m{item_table}\033[39m: {row[item_table]}, ', end='')
+
+
     pass
+
+
+def clear_database(my_db_tables):
+    try:
+        with psycopg2.connect(host=DB_HOST, database=DB_BASE,
+                              user=DB_USER, password=DB_PASSWORD) as conn_sql:
+            # print(conn_sql.status)
+            with conn_sql.cursor() as cur_sql:
+                # print(cur_sql.statusmessage)
+                # print(my_db_tables)
+                for table in my_db_tables.split(', '):
+                    cur_sql.execute(f'SELECT COUNT(*) FROM {table}')
+
+                    print(f'Очищаем таблицу \033[34m{table}\033[39m '
+                          f'(количество записей = \033[34m{cur_sql.fetchone()[0]}\033[39m)'
+                          f' в базе данных \033[33m{DB_BASE.upper()}\033[39m')
+
+                    cur_sql.execute(f'DELETE FROM {table}')
+
+            conn_sql.commit()
+
+    finally:
+        conn_sql.close()
 
 
 def main():
@@ -59,10 +90,15 @@ def main():
             print('\n\t\t ---= Пока =---')
             break
         elif user_input == '1':
-            pass
+            my_db_tables = ', '.join(table['table'].upper() for table in TABLES)
+
+            # print('{0:^50}'.format(f'Очищаем таблицы '
+            #                        f'\033[33m{my_db_tables}\033[39m '
+            #                        f'в базе данных \033[33m{DB_BASE.upper()}\033[39m'))
+            clear_database(my_db_tables)
 
         elif user_input == '2':
-            print('{0:^30}'.format('Грузим данные с CSV в базу данных'))
+            print('{0:^50}'.format('Грузим данные с CSV в базу данных'))
             for table in TABLES:
                 print(
                     f"из файла \033[32m{table['file_name']}\033[39m в таблицу "
