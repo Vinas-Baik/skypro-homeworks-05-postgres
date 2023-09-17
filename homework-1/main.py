de91744c1867f99847d479f6793b4621c324f36b
Victor Sadykov <svn1409@gmail.com>
2023-09-17 22:47:24 +0800

    homework 1 add docstrings and comments

--- homework-1/main.py
+++ homework-1/main.py
@@ -2,10 +2,13 @@
 
 import psycopg2
 import csv
-import os
 
 from utils.svn_utils import *
 
+# Описание таблиц
+# file_name - имя файла
+# table - имя таблицы
+# values - список колонок, столбцов, атрибутов  таблицы
 TABLES = [{'file_name': os.getcwd() + '\\north_data\\customers_data.csv',
            'table': 'customers',
            'values': 'customer_id, company_name, contact_name',
@@ -23,6 +26,7 @@ TABLES = [{'file_name': os.getcwd() + '\\north_data\\customers_data.csv',
            }
           ]
 
+# параметры для подключения к базе данных
 DB_HOST = 'localhost'
 DB_BASE = 'north'
 DB_USER = 'postgres'
@@ -30,6 +34,9 @@ DB_PASSWORD = '12345'
 
 
 def user_menu() -> str:
+    """
+    меню выводимое для пользователя с запросом от пользователя выбора пункта меню
+    """
     print('\n -----= Меню =----- ')
     print('\t\033[32m[1]\033[39m - Очистить таблицы в базе данных')
     print('\t\033[32m[2]\033[39m - считать данные из файлов в таблицы')
@@ -46,6 +53,9 @@ def user_menu() -> str:
     return user_input
 
 def count_items_table(table_name: str) -> int:
+    """
+    подсчет количества записей в таблице базы данных
+    """
     try:
         with psycopg2.connect(host=DB_HOST, database=DB_BASE,
                               user=DB_USER,
@@ -62,6 +72,9 @@ def count_items_table(table_name: str) -> int:
 
 
 def load_from_csv_in_table(table):
+    """
+    грузим данные с CSV файла в таблицу
+    """
 
     try:
         with psycopg2.connect(host=DB_HOST, database=DB_BASE,
@@ -73,9 +86,11 @@ def load_from_csv_in_table(table):
                     rows = csv.reader(file)
                     count_row_csv = 0
                     for row in rows:
+                        # пропускаем 1 строчку из CSV файла
                         if count_row_csv != 0:
                             # print(f'Оригинал - {row}')
                             tuple_row = []
+                            # преобразуем символ ' в символ `, так есть проблемы с работой команды INSERT
                             for t_value in row:
                                 tuple_row.append(t_value.replace('\'', '`'))
 
@@ -85,6 +100,8 @@ def load_from_csv_in_table(table):
                         count_row_csv += 1
 
                 conn_sql.commit()
+
+                # запрашиваем количество записей в таблице
 
                 cur_sql.execute(f'SELECT COUNT(*) FROM {table["table"]}')
                 print(f'Добавлено в таблицу \033[34m{table["table"]}\033[39m '
@@ -96,13 +113,15 @@ def load_from_csv_in_table(table):
 
 
 def clear_database(my_db_tables):
+    """
+    очищаем таблицу от записей
+    my_db_tables - передаем список таблиц через запятую
+    """
     try:
         with psycopg2.connect(host=DB_HOST, database=DB_BASE,
                               user=DB_USER, password=DB_PASSWORD) as conn_sql:
             # print(conn_sql.status)
             with conn_sql.cursor() as cur_sql:
-                # print(cur_sql.statusmessage)
-                # print(my_db_tables)
                 for table in my_db_tables.split(', '):
                     cur_sql.execute(f'SELECT COUNT(*) FROM {table}')
 
@@ -119,7 +138,9 @@ def clear_database(my_db_tables):
 
 
 def main():
-
+    """
+    главная часть программы
+    """
     while True:
         user_input = user_menu()
         if user_input == '0':
@@ -135,20 +156,24 @@ def main():
             clear_database(my_db_tables[:-2])
 
         elif user_input == '2':
+            # Грузим данные из файлов CSV
             print('{0:^50}'.format('Грузим данные с CSV в базу данных'))
             for table in TABLES:
                 count_items = count_items_table(table['table'])
+                # проверяем количество записей в таблице
                 if count_items != 0:
                     print(f'\033[31mТаблица {table["table"].upper()} содержит '
                           f'{count_items} записей \033[39m')
                     print(f'Таблицу {table["table"]} нужно очистить '
                           f'перед добавлением записей из файла')
                 else:
+                    # работаем только с пустой таблицей
                     print(f"из файла \033[32m{table['file_name']}\033[39m в таблицу "
                           f"\033[33m{table['table'].upper()}\033[39m")
                     load_from_csv_in_table(table)
 
         elif user_input == '3':
+            # вывод количества записей в таблицах
             for table in TABLES:
                 count_items = count_items_table(table['table'])
                 print(f'Таблица \033[32m{table["table"].upper()}\033[39m содержит '
